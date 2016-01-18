afd54da5a07412cd58f031aab5686b65dac67281
Adrian Olariu <olariuadrian@gmail.com>
2016-01-18 16:19:38 +0000

    read events next page

--- methods/event_read.js
+++ methods/event_read.js
@@ -2,31 +2,57 @@ var _ = require('underscore');
 
 var Promise = require('bluebird');
 var request = require('request');
+//request.debug = true;
 
 var Errors = require('../errors');
 
 module.exports = function(params) {
     return new Promise(function(resolve, reject) {
+        var events = [];
+
         params['tzid'] = 'Etc/UTC';
 
         var options = {
-            uri: "https://api.cronofy.com/v1/events",
-            method: 'GET',
-            headers: {
-                'Content-type': 'application/json; charset=utf-8',
-                'Authorization': 'Bearer ' + params['access_token']
-            },
-            qs: _.omit(params, 'access_token'),
-            json: true
-        };
+                uri: "https://api.cronofy.com/v1/events",
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json; charset=utf-8',
+                    'Authorization': 'Bearer ' + params['access_token']
+                },
+                qs: _.omit(params, 'access_token'),
+                json: true
+            };
 
-        request(options, function (error, response, body) {
+        request(options, responseCallback);
+
+        function responseCallback(error, response, body) {
+            console.log('requestCallback');
             if ( error || (response.statusCode != 200 && response.statusCode != 202) ) {
                 console.log(error, response.statusCode);
                 return reject(error);
             }
-            
-            return resolve(body.events);
-        });
+
+            events.push(body.events);
+
+            nextPageFn(body.pages && body.pages.next_page);
+        }
+
+        function nextPageFn(nextPageUrl) {
+            console.log('nextPageUrl ', nextPageUrl);
+            if (!nextPageUrl) return resolve(_.flatten(events));
+
+            request(
+                {
+                    uri: nextPageUrl,
+                    method: 'GET',
+                    headers: {
+                        'Content-type': 'application/json; charset=utf-8',
+                        'Authorization': 'Bearer ' + params['access_token']
+                    },
+                    json: true
+                },
+                responseCallback
+            );
+        }
     });
 };
